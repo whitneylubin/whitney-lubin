b6d012fc718c25fbd401b06490cdfd6fd63db8c3
Whitney Lubin <whitneylubin@icloud.com>
2018-12-27 21:25:08 -0500

    add projects

--- src/components/PortfolioSection.js
+++ src/components/PortfolioSection.js
@@ -8,22 +8,27 @@ const portfolioItems = [
   {
     imgSrc: "../images/portfolio-1.jpg",
     heading: "Resume Website",
-    description: "React.js Portfolio Website"
+    description: "React.js Portfolio Website",
+    link: "https://github.com/whitneylubin?tab=repositories"
   },
   {
     imgSrc: "../images/portfolio-2.jpg",
     heading: "Break.in",
-    description: "React-on-Rails Web Application"
+    description: "React-on-Rails Web Application",
+    link: "https://github.com/whitneylubin?tab=repositories"
   },
   {
     imgSrc: "../images/portfolio-3.jpg",
-    heading: "To-Do List",
-    description: "Ruby on Rails To-Do List"
+    heading: "Friendly Chat Web Client",
+    description:
+      "Sync data using the Firebase Realtime Database and Cloud Storage for Firebase. Authenticate users using Firebase Auth. Deployed web app on Firebase Hosting. Send notifications with Firebase Cloud Messaging.",
+    link: "https://github.com/whitneylubin/friendlychat-web-1"
   },
   {
     imgSrc: "../images/portfolio-4.jpg",
-    heading: "Weather App",
-    description: "React.js Weather App"
+    heading: "React Weather App",
+    description: "Bootstrap React Application using create-react-app script",
+    link: "https://github.com/whitneylubin?tab=repositories"
   }
 ];
 
@@ -33,8 +38,8 @@ const PortfolioSection = () => (
       <div className="row no-gutters">
         {portfolioItems.map((project, index) => (
           <div className="col-lg-6" key={`portfolio_item_${index}`}>
-            <img className="img-fluid" src={project.imgSrc} alt="" />
-            <a className="portfolio-item" href="">
+            <a className="portfolio-item" href={project.link}>
+              <img className="img-fluid" src={project.imgSrc} alt="" />
               <span className="caption">
                 <span className="caption-content">
                   <h2>{project.heading}</h2>
