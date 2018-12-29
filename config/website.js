cb2a0f7ef4e88996ffc416bc77e2de795efa5b19
Whitney Lubin <whitneylubin@icloud.com>
2018-12-29 12:36:47 -0500

    website.js

--- config/website.js
+++ config/website.js
@@ -1,21 +1,21 @@
-const tailwind = require('../tailwind');
+const tailwind = require("../tailwind");
 
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-cara.netlify.com', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  siteTitle: "Whitney - Gatsby Starter Portfolio", // Navigation and Site Title
+  siteTitleAlt: "Whitney", // Alternative Site title for SEO
+  siteUrl: "https://whitneylubin.github.io/whitney-lubin", // Domain of your site. No trailing slash!
+  siteLanguage: "en", // Language Tag on <html> element
+  siteLogo: "/logos/logo-1024.png", // Used for SEO and manifest
+  siteDescription: "Playful & Colorful One-Page website with Parallax effect",
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
-  ogLanguage: 'en_US', // Facebook Language
+  userTwitter: "@whitney.lubin", // Twitter Username
+  ogSiteName: "Whitney", // Facebook Site Name
+  ogLanguage: "en_US", // Facebook Language
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
-  backgroundColor: tailwind.colors.blue,
+  backgroundColor: tailwind.colors.blue
 };
