b5fddd1a7805b8b51a2243ebd86e1745c29acd78
lubin <whitneylubin@icloud.com>
2019-03-02 13:08:35 -0500

    changed title

--- config/website.js
+++ config/website.js
@@ -3,17 +3,16 @@ const tailwind = require("../tailwind");
 module.exports = {
   pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: "Whitney - Gatsby Starter Portfolio", // Navigation and Site Title
-  siteTitleAlt: "Whitney", // Alternative Site title for SEO
+  siteTitle: "Whitney Lubin", // Navigation and Site Title
+  siteTitleAlt: "Whitney Lubin Software Engineer", // Alternative Site title for SEO
   siteUrl: "https://whitneylubin.github.io/whitney-lubin", // Domain of your site. No trailing slash!
   siteLanguage: "en", // Language Tag on <html> element
   siteLogo: "/logos/logo-1024.png", // Used for SEO and manifest
-  siteDescription: "Playful & Colorful One-Page website with Parallax effect",
+  siteDescription: "PortFolio Site for Whitney Lubin",
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: "@cara", // Twitter Username
-  ogSiteName: "cara", // Facebook Site Name
-  ogLanguage: "en_US", // Facebook Language
+  userTwitter: "@thewhitneylubin", // Twitter Username
+
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
